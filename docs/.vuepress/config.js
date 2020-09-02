b274b53e928753cddaabccf7ea8207673961ee9e
YUANYU <970072249@qq.com>
2020-09-02 17:05:14 +0800

    first

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: 'yuan',
+  description: '这是钮旭明的测试项目啊哈哈',
+  base: '/vuepress-demo/',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@alias': './assets/'
+      }
+    }
+  },
+  dest: './dist',
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/' },
+      { text: 'Blog', link: '/blog/' },
+      { text: '百度', link: 'https://www.baidu.com' },
+      { text: 'About', link: '/about/' },
+    ],
+    sidebar: [
+      {
+        title: 'Blog',
+        collapsable: false,
+        children: [
+          '/blog/',
+          '/blog/first'
+        ]
+      }
+    ],
+    sidebarDepth: 3
+  }
+}
